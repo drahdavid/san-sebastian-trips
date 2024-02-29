ee6c6713c59ccb185ad5a55be1f15a3919a4e627
David Drah Llensa <llensa@fktech.net>
2024-02-29 10:16:46 -0300

    Se añade cambio en captcha

--- src/app/components/Captcha.js
+++ src/app/components/Captcha.js
@@ -4,9 +4,8 @@ import ReCAPTCHA from "react-google-recaptcha";
 export const Captcha = () => {
   return (
     <ReCAPTCHA
-      //   ref={recaptchaRef}
-      //   sitekey={process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
-      sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+    //   ref={recaptchaRef}
+      sitekey={process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
       onChange={(e) => console.log(e)}
     />
   );
